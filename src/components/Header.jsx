72f0e18ba0e7df98aebfa03416e21faa3edd0d42
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-06-03 21:00:25 +0200

    ✨feat: Add the header menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
+import { Icon } from "@iconify/react";
 
+import { dataMenu } from "../data/dataMenu";
 
 const Header = () => {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  const handleViewMenu = () => {
+    setIsMenuVisible(!isMenuVisible);
+  };
+
   return (
     <header className="header">
-        <div className="grid-2-smaill header_menu">
-            <div className="header_menu">
-                
-            </div>
-
+      <div className="grid-2-small header__menu">
+        <div className="header__icon">
+          <Icon icon="mdi:chevron-left" width={25} />
         </div>
+        <div className="header__icon-right">
+          <Icon icon="mdi:cards-heart-outline" width={25} />
+          <Icon
+            onClick={handleViewMenu}
+            icon="carbon:overflow-menu-vertical"
+            width={25}
+            style={{ color: "black" }}
+          />
+        </div>
+        { isMenuVisible   && (
+          <ul className="menu-top">
+            {dataMenu.map((item) => (
+              <li key={item.id} className="menu-top__item">
+                <a href="#" className="menu-top__link">
+                  <Icon icon={item.icon} width={25} />
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/data/dataMenu.js
@@ -0,0 +1,33 @@
+export const dataMenu = [
+    {
+        id: 1,
+        name: 'Nuestras canciones',
+        icon:"mdi:music"
+    },
+    {
+        id: 2,
+        name: 'Preboda',
+        icon:"mdi:glass-tulip"
+    },
+    {
+        id: 3,
+        name: 'Ceremonia',
+        icon:"mdi:church-outline"
+    },
+    {
+        id: 4,
+        name: 'Celebración',
+        icon:"mdi:food"
+    },
+    {
+        id: 5,
+        name: 'Hoteles',
+        icon:"mdi:hotel"
+    },
+    {
+        id: 6,
+        name: 'Confirmación',
+        icon:"mdi:check"
+    },
+
+];
