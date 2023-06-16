bb8e1dc59022a9e1f8ef8221cb4b1d23759bba8a
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-06-16 20:17:36 +0200

    ✨feat: add  new  info in leisure

--- src/components/LeisureSection.jsx
+++ src/components/LeisureSection.jsx
@@ -20,15 +20,15 @@ const LeisureSection = () => {
           />
         </div>
         <div className="leisure__text">
-          <h3>PREBODA Viernes 15 de Sep. 19:00h</h3>
-          <h4>WHITE PARTY · Cortijo La Loma </h4>
-          <a href="https://www.google.com/maps/place/36%C2%B049'12.6%22N+2%C2%B003'04.5%22W/@36.820179,-2.051257,17z/data=!3m1!4b1!4m4!3m3!8m2!3d36.820179!4d-2.051257?entry=ttu" 
-          target="_blank"
-          rel="noreferrer"
-          className="leisure__text-link"
+          <h3>PREBODA Viernes 15 Sep. 19:00h.</h3>
+          <h4>WHITE PARTY · Café del Cabo</h4>
+          <a
+            href="https://www.google.com/maps/place/36%C2%B049'12.6%22N+2%C2%B003'04.5%22W/@36.820179,-2.051257,17z/data=!3m1!4b1!4m4!3m3!8m2!3d36.820179!4d-2.051257?entry=ttu"
+            target="_blank"
+            rel="noreferrer"
+            className="leisure__text-link"
           >
             Cortijo La Loma, 04118 La Isleta Del moro <br />
-           
           </a>
           <span className="dresscode">Dresscode: ropa blanca</span>
         </div>
@@ -47,14 +47,14 @@ const LeisureSection = () => {
         </div>
         <div className="leisure__text">
           <h3>CEREMONIA Sábado 16 Sep. 18:30h</h3>
-          
-          <a href="https://www.google.com/maps/place/36%C2%B045'22.6%22N+2%C2%B007'18.1%22W/@36.756278,-2.12169,17z/data=!3m1!4b1!4m4!3m3!8m2!3d36.756278!4d-2.12169?entry=ttu"
-          target="_blank"
-          rel="noreferrer"
-          className="leisure__text-link"
+
+          <a
+            href="https://www.google.com/maps/place/36%C2%B045'22.6%22N+2%C2%B007'18.1%22W/@36.756278,-2.12169,17z/data=!3m1!4b1!4m4!3m3!8m2!3d36.756278!4d-2.12169?entry=ttu"
+            target="_blank"
+            rel="noreferrer"
+            className="leisure__text-link"
           >
-            Era de los Genoveses,04118 San josé, Almeria  <br />
-            
+            Era de los Genoveses,04118 San josé, Almeria <br />
           </a>
           <span className="dresscode">Se recomienda no llevar tacón fino</span>
         </div>
@@ -74,16 +74,15 @@ const LeisureSection = () => {
         <div className="leisure__text">
           <h3>CELEBRACIÓN</h3>
           <h4>Cortijo La Loma </h4>
-          <a href="https://www.google.com/maps/place/36%C2%B049'12.6%22N+2%C2%B003'04.5%22W/@36.820179,-2.051257,17z/data=!3m1!4b1!4m4!3m3!8m2!3d36.820179!4d-2.051257?entry=ttu"
-          target="_blank"
-          rel="noreferrer"
-          className="leisure__text-link"
+          <a
+            href="https://www.google.com/maps/place/36%C2%B049'12.6%22N+2%C2%B003'04.5%22W/@36.820179,-2.051257,17z/data=!3m1!4b1!4m4!3m3!8m2!3d36.820179!4d-2.051257?entry=ttu"
+            target="_blank"
+            rel="noreferrer"
+            className="leisure__text-link"
           >
             Cortijo La Loma, 04118 La Isleta Del moro <br />
-            
           </a>
           <span className="dresscode">Se recomienda no llevar tacón fino</span>
-          
         </div>
         <div>
           <img src={reproductor} alt="" className="reproductormusica" />
@@ -102,8 +101,9 @@ const LeisureSection = () => {
           <h3>HOTEL MC SAN JOSÉ</h3>
           <h4>C. del Faro, 2, 04118 San José, Almería </h4>
           <p>
-          <a className="phone" href="tel:+34678499972">(+34) 678 49 99 72</a>
-
+            <a className="phone" href="tel:+34678499972">
+              (+34) 678 49 99 72
+            </a>
           </p>
         </div>
         <div>
@@ -123,7 +123,9 @@ const LeisureSection = () => {
           <h3>HOTEL DOÑA PAKYTA</h3>
           <h4>C. Correo, 51, 04118 San José, Almería</h4>
           <p>
-            <a className="phone" href="tel:950611175" >950 61 11 75</a>
+            <a className="phone" href="tel:950611175">
+              950 61 11 75
+            </a>
           </p>
         </div>
         <div>
